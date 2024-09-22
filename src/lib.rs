9298f78af297cf8dedf66804b015051df49cfd72
Ionut <pialex2032@gmail.com>
2024-09-22 23:30:31 +0300

    rename

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use include_dir::Dir;
 const Z80_TESTS: Dir<'_> = include_dir::include_dir!("z80/v1");
 
-pub fn get_test(name: &str) -> Option<&'static str> {
+pub fn get_z80_tests(name: &str) -> Option<&'static str> {
     Z80_TESTS.get_file(name).map(|f| f.contents_utf8().unwrap())
 }
