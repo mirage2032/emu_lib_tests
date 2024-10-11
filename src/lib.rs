6bc901f96d47a6155b2957a7f8efd19ce3e0f5d0
Ionut Alexandru Popescu <pialex2032@gmail.com>
2024-10-11 04:56:30 +0300

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use include_dir::Dir;
-const Z80_TESTS: Dir<'_> = include_dir::include_dir!("z80/v1");
+use include_directory::{include_directory, Dir};
+
+const Z80_TESTS: Dir<'_> = include_directory!("z80/v1");
 
 pub fn get_z80_tests(name: &str) -> Option<&'static str> {
     Z80_TESTS.get_file(name).map(|f| f.contents_utf8().unwrap())
